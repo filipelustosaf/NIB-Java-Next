517f25cba6e2cd0b5733cdf5ee0a3dd5e360bcd7
Filipe <filipefranca2@gmail.com>
2021-09-30 22:24:14 -0300

    Commit

--- src/main/java/NIBMain.java
+++ src/main/java/NIBMain.java
@@ -1,10 +1,21 @@
 
 import guis.ClienteGui;
+import guis.ContaGui;
+
+import java.util.Scanner;
 
 public class NIBMain {
 
     public static void main(String[] args) {
+        Scanner leTeclado = new Scanner(System.in);
+
         ClienteGui clienteGui = new ClienteGui();
-        clienteGui.exibeMenu();
+        clienteGui.exibeMenu(leTeclado);
+
+        ContaGui contaGui = new ContaGui();
+        contaGui.exibeMenu(leTeclado);
+
+        System.out.println("Obrigado por usar o NIB");
+        leTeclado.close();
     }
 }
